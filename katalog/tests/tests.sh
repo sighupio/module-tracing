2258ce2e46bb7d54de871a0d7ea6be80ed605db9
Samuele Chiocca <samuele@sighup.io>
2023-11-10 14:52:42 +0100

    feat: update e2e test script

--- katalog/tests/tests.sh
+++ katalog/tests/tests.sh
@@ -15,16 +15,29 @@ set -o pipefail
   kubectl apply -f https://raw.githubusercontent.com/sighupio/fury-kubernetes-monitoring/v2.0.1/katalog/prometheus-operator/crds/0prometheusruleCustomResourceDefinition.yaml
   kubectl apply -f https://raw.githubusercontent.com/sighupio/fury-kubernetes-monitoring/v2.0.1/katalog/prometheus-operator/crds/0servicemonitorCustomResourceDefinition.yaml
   kubectl apply -f https://raw.githubusercontent.com/sighupio/fury-kubernetes-monitoring/v2.0.1/katalog/prometheus-operator/crds/0probeCustomResourceDefinition.yaml
-}
-
-@test "testing tempo apply" {
-  info
-  apply katalog/tempo-distributed
+  kubectl create ns tracing
 }
 
 @test "testing minio-ha apply" {
   info
   apply katalog/minio-ha
+}
+
+@test "wait for apply to settle and dump state to dump.json" {
+  info
+  max_retry=0
+  echo "=====" $max_retry "=====" >&2
+  while kubectl get pods --all-namespaces | grep -ie "\(Pending\|Error\|CrashLoop\|ContainerCreating\|PodInitializing\)" >&2
+  do
+    [ $max_retry -lt 30 ] || ( kubectl describe all --all-namespaces >&2 && return 1 )
+    sleep 10 && echo "# waiting..." $max_retry >&3
+    max_retry=$((max_retry+1))
+  done
+}
+
+@test "testing tempo apply" {
+  info
+  apply katalog/tempo-distributed
 }
 
 @test "wait for apply to settle and dump state to dump.json" {
@@ -59,17 +72,4 @@ set -o pipefail
   loop_it test 60 5
   status=${loop_it_result}
   [[ "$status" -eq 0 ]]
-}
-
-@test "cleanup" {
-  if [ -z "${LOCAL_DEV_ENV}" ];
-  then
-    skip
-  fi
-  for dir in minio-ha tempo-distributed
-  do
-    echo "# deleting katalog/$dir" >&3
-    kustomize build katalog/$dir | kubectl delete -f - || true
-    echo "# deleted katalog/$dir" >&3
-  done
 }
